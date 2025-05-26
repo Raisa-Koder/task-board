75d1dec3cfc76429ace4ad842f73f4bfba4c0898
Raisa <raisakoder03@gmail.com>
2025-05-26 19:58:40 +0530

    `feat: added default task name and updated task model for validaiton`

--- server/src/models/Task.js
+++ server/src/models/Task.js
@@ -1,22 +1,31 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    description: String,
-    status: {
-      type: String,
-      enum: ["In Progress", "Completed", "Won't Do"],
-      default: "In Progress",
-    },
-    icon: String,
-    board: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Board",
-      required: true,
-    },
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Task name is required"],
+    trim: true,
+    minlength: 3,
+    maxlength: 100,
   },
-  { timestamps: true }
-);
+  description: {
+    type: String,
+    default: "",
+  },
+  icon: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    enum: ["In Progress", "Completed", "Won't Do"],
+    default: "In Progress",
+  },
+  boardId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Board",
+    required: true,
+  },
+}, { timestamps: true });
 
 module.exports = mongoose.model("Task", taskSchema);
